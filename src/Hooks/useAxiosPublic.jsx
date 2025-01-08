8e84981c62adbe97eb95f91f0f783a0eec85c2dc
kanto <kanto@KANTO-PC>
2025-01-09 01:52:04 +0600

    u

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL:'https://server-allaboutcraftbd.vercel.app'
+    baseURL:'https://all-about-back.vercel.app'
 })
 const useAxiosPublic = () => {
     return axiosPublic;
